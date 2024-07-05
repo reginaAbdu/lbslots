1b595175becd75612ff9a2f96e1f43f31872b5da
reginaAbdu <98775919+reginaAbdu@users.noreply.github.com>
2024-07-05 16:31:46 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Leasebreak Featured Slots</title>
+</head>
+<body>
+    <p id="text-content">Loading...</p>
+
+    <script src="script.js"></script>
+</body>
+</html>
